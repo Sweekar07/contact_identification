6c3265d86aceda5990f715af999df4d47015ca73
Sweekar Bangera <sweekar.786b@gmail.com>
2026-02-28 13:48:08 +0530

    refac: using aiven postgres db

--- app/database.py
+++ app/database.py
@@ -1,4 +1,5 @@
 from tortoise import Tortoise
+import ssl
 import os
 
 ENV = os.getenv("ENV", "prod")
@@ -8,9 +9,11 @@ else:
     DATABASE_URL = os.getenv("DATABASE_URL")
 
 async def init_db():
+    ssl_context = ssl.create_default_context()
     await Tortoise.init(
         db_url=DATABASE_URL,
-        modules={"models": ["app.models"]}
+        modules={"models": ["app.models"]},
+        ssl=ssl_context
     )
     await Tortoise.generate_schemas()
 
