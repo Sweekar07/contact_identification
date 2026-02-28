1351ad5d8eb12843eca90fcabc80ffcc451b9943
Sweekar07 <sweekar.786b@gmail.com>
2026-02-28 15:58:05 +0530

    refac: init db

--- app/database.py
+++ app/database.py
@@ -1,5 +1,4 @@
 from tortoise import Tortoise
-import ssl
 import os
 
 ENV = os.getenv("ENV", "prod")
@@ -9,11 +8,9 @@ else:
     DATABASE_URL = os.getenv("DATABASE_URL")
 
 async def init_db():
-    ssl_context = ssl.create_default_context()
     await Tortoise.init(
         db_url=DATABASE_URL,
         modules={"models": ["app.models"]},
-        ssl=ssl_context
     )
     await Tortoise.generate_schemas()
 
